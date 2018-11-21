bd339c0640926bc20a37690fef56be491ba8e3f6
Madina Nurgozhina <develop.profi@gmail.com>
2018-11-21 20:18:19 +0600

    Добавьте файлы проекта.

---
+++ CarApplication/Pages/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarApplication.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+      
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mf.Source = new Uri("Pages/MainPageRef.xaml", UriKind.RelativeOrAbsolute);
+            MainPageRef mpr = new MainPageRef();
+            MainWindow.mf.NavigationService.Navigate(mpr);            
+        }
+    }
+}
---
+++ CarApplication/Pages/MainPageRef.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CarApplication.Pages;
+
+namespace CarApplication.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPageRef.xaml
+    /// </summary>
+    public partial class MainPageRef : Page
+    {
+        public MainPageRef()
+        {
+            InitializeComponent();
+        }       
+
+        
+        private void tbxVIN_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tbxVIN.Text = null;
+            tbxVIN.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void tbx1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tbx1.Text = null;
+            tbx1.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void tbx2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tbx2.Text = null;
+            tbx2.Foreground = new SolidColorBrush(Colors.Black);
+        }
+    }
+
+
+}
